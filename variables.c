78e7abd36b1adda31ba48ee1cdc014c14f5541d5
yahya <yahyayuri10@gmail.com>
2025-05-21 10:52:52 +0100

    c beginer with vs code

---
+++ variables.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+int main()
+{
+    float somme;
+    float module;
+
+    printf("Entre la somme : ");
+    scanf("%f", &somme);
+    printf("Entre les modules : ");
+    scanf("%f", &module);
+    printf(" La moyenne est : %f", somme/module);
+
+    return 0;
+
+}
+  
