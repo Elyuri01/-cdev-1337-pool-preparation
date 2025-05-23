7c027181e54c08efce0ab33ebcc0752356fea0cc
Elyahya01 <yahyayuri10@gmail.com>
2025-05-23 06:15:28 +0100

    ex01part1.c

---
+++ ex01part1.c
@@ -0,0 +1,13 @@
+# include <stdio.h>
+
+//write one printf statement that displays many text with new lines
+
+int main()
+{
+   printf("I am learning Programming\n");
+   printf("With\n");
+   printf("Dev\n");
+   printf("Journey\n");
+   printf("1337\n");
+   return 0;
+}
