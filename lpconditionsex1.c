0d6eb67d6c722d7c123ab4c5babd0afaca42c711
Elyahya01 <yahyayuri10@gmail.com>
2025-05-31 19:05:04 +0100

    loops and conditions combined lessons ex1

---
+++ lpconditionsex1.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+// Write a program that displays all even numbers from 1 to 20.
+
+int main(void) {
+    
+    int i = 0;
+    
+    while (i < 20) {
+	    i++;
+	    if (i % 2 == 0) {
+                    printf("%d\n", i);
+	    }
+	    else {
+	    }
+    }
+    return 0;
+}
+     
